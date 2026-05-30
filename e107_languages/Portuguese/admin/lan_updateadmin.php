03d1bc87c586b4fd31472c7a4e514f93e307b4a6
rica-carv <rica-carv@users.noreply.github.com>
2026-05-30 14:38:44 +0100

    Add files via upload

--- e107_languages/Portuguese/admin/lan_updateadmin.php
+++ e107_languages/Portuguese/admin/lan_updateadmin.php
@@ -1,13 +1,11 @@
 <?php
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("UDALAN_1", "Erro - por favor, envie-o novamente.");
-define("UDALAN_2", "Configurações de admin atualizado");
-define("UDALAN_3", "Configurações atualizadas para");
-define("UDALAN_4", "Nome");
-define("UDALAN_6", "Re-digite a senha");
-define("UDALAN_7", "Alterar senha");
-define("UDALAN_8", "Senha atualizada para");
+return [
+    'UDALAN_1' => "Erro - por favor, envie-o novamente.",
+    'UDALAN_2' => "Configurações de admin atualizado",
+    'UDALAN_3' => "Configurações atualizadas para",
+    'UDALAN_4' => "Nome",
+    'UDALAN_6' => "Re-digite a senha",
+    'UDALAN_7' => "Alterar senha",
+    'UDALAN_8' => "Senha atualizada para",
+];
--- e107_languages/Portuguese/admin/lan_userclass2.php
+++ e107_languages/Portuguese/admin/lan_userclass2.php
@@ -1,59 +1,49 @@
 <?php
-/*
-+---------------------------------------------------------------+
-|        e107 sítio internet content management system Portuguese Language File
-|        Released under the terms and conditions of the
-|        GNU General Public License (http://gnu.org).
-|        Last Modified: 2021/02/14 20:24:21
-|
-|        $Author: Ricardo $
-+---------------------------------------------------------------+
-*/
 
-define("UCSLAN_30", "Nome curto exibido em seletores");
-define("UCSLAN_31", "Informações sobre a aplicabilidade da classe");
-define("UCSLAN_32", "utilizadores nesta classe podem adicionar/remover próprios da classe sendo editado");
-define("UCSLAN_33", "Determina quais utilizadores podem ver essa classe em listas drop-down");
-define("UCSLAN_36", "Se o topo da árvore é 'Ninguém', permissões aumentam em direção ao topo da árvore<br>Se o topo da árvore é 'Todos', permissões aumentam à medida que você descer da árvore");
-define("UCSLAN_37", "Você deve digitar um nome para a classe");
-define("UCSLAN_38", "Classe de utilizador inicial");
-define("UCSLAN_39", "Não há classes que podem ser definidas");
-define("UCSLAN_40", "Conjunto de classes iniciais");
-define("UCSLAN_41", "Configurações atualizadas");
-define("UCSLAN_43", "Classes existentes:");
-define("UCSLAN_45", "Ponto em que classes de conjunto:");
-define("UCSLAN_46", "(ignorado se não verificação)");
-define("UCSLAN_47", "Inscrição inicial");
-define("UCSLAN_48", "Verificação por endereço electrónico ou admin");
-define("UCSLAN_49", "Essas classes são definidas para qualquer utilizador recentemente se inscreveu - associação quer imediatamente, ou uma vez seu site foi verificada");
-define("UCSLAN_53", "Cuidado! Só usar essas opções se você entende o que eles fazem");
-define("UCSLAN_54", "Definir uma hierarquia de utilizador padrão");
-define("UCSLAN_55", "Limpar a hierarquia de utilizador");
-define("UCSLAN_56", "(isso define uma estrutura de classe de utilizador 'plana')");
-define("UCSLAN_57", "(a hierarquia pode ser modificada mais tarde)");
-define("UCSLAN_58", "Executar");
-define("UCSLAN_62", "Crie a árvore de classe padrão:");
-define("UCSLAN_63", "Esse nome de classe já existe - por favor escolha outro");
-define("UCSLAN_64", "concluído");
-define("UCSLAN_65", "Achate a hierarquia de classes de utilizador:");
-define("UCSLAN_69", "Opcional ícone associado com classe - diretório");
-define("UCSLAN_70", "Reconstruindo a hierarquia de classe:");
-define("UCSLAN_71", "Manutenção de classe de utilizador");
-define("UCSLAN_72", "Reconstruir a hierarquia de classes");
-define("UCSLAN_73", "(Pode ser necessária se ocorrer corrupção de banco de dados)");
-define("UCSLAN_74", "Os administradores e moderadores");
-define("UCSLAN_75", "Membros registrados e logados");
-define("UCSLAN_76", "Administradores do site");
-define("UCSLAN_77", "Os administradores de site principal");
-define("UCSLAN_78", "Moderadores de fóruns e outras áreas");
-define("UCSLAN_80", "Padrão");
-define("UCSLAN_81", "Grupo");
-define("UCSLAN_82", "Um grupo reúne uma série de aulas individuais");
-define("UCSLAN_83", "Aulas em grupo");
-define("UCSLAN_85", "Você tem atribuído a todas as classes disponíveis; por favor, reatribuir um que não está em uso");
-define("UCSLAN_86", "Algumas configurações não permitida para classes de admin - eles tiverem sido definidos para os padrões.");
-define("UCSLAN_87", "Recentemente se juntou aos utilizadores");
-define("UCSLAN_88", "Robôs de busca identificados");
-define("UCSLAN_89", "Verificado as classes são membros do grupo");
-define("UCSLAN_90", "Você não pode editar certas classes de utilizador do sistema!");
-define("UCSLAN_91", "Estrutura da classe");
+return [
+    'UCSLAN_30' => "Nome curto exibido em seletores",
+    'UCSLAN_31' => "Informações sobre a aplicabilidade da classe",
+    'UCSLAN_32' => "utilizadores nesta classe podem adicionar/remover próprios da classe sendo editado",
+    'UCSLAN_33' => "Determina quais utilizadores podem ver essa classe em listas drop-down",
+    'UCSLAN_37' => "Você deve digitar um nome para a classe",
+    'UCSLAN_38' => "Classe de utilizador inicial",
+    'UCSLAN_39' => "Não há classes que podem ser definidas",
+    'UCSLAN_40' => "Conjunto de classes iniciais",
+    'UCSLAN_41' => "Configurações atualizadas",
+    'UCSLAN_43' => "Classes existentes:",
+    'UCSLAN_45' => "Ponto em que classes de conjunto:",
+    'UCSLAN_46' => "(ignorado se não verificação)",
+    'UCSLAN_47' => "Inscrição inicial",
+    'UCSLAN_48' => "Verificação por endereço electrónico ou admin",
+    'UCSLAN_49' => "Essas classes são definidas para qualquer utilizador recentemente se inscreveu - associação quer imediatamente, ou uma vez seu site foi verificada",
+    'UCSLAN_53' => "Cuidado! Só usar essas opções se você entende o que eles fazem",
+    'UCSLAN_54' => "Definir uma hierarquia de utilizador padrão",
+    'UCSLAN_55' => "Limpar a hierarquia de utilizador",
+    'UCSLAN_57' => "(a hierarquia pode ser modificada mais tarde)",
+    'UCSLAN_58' => "Executar",
+    'UCSLAN_62' => "Crie a árvore de classe padrão:",
+    'UCSLAN_63' => "Esse nome de classe já existe - por favor escolha outro",
+    'UCSLAN_64' => "concluído",
+    'UCSLAN_65' => "Achate a hierarquia de classes de utilizador:",
+    'UCSLAN_69' => "Opcional ícone associado com classe - diretório",
+    'UCSLAN_70' => "Reconstruindo a hierarquia de classe:",
+    'UCSLAN_71' => "Manutenção de classe de utilizador",
+    'UCSLAN_72' => "Reconstruir a hierarquia de classes",
+    'UCSLAN_73' => "(Pode ser necessária se ocorrer corrupção de banco de dados)",
+    'UCSLAN_74' => "Os administradores e moderadores",
+    'UCSLAN_75' => "Membros registrados e logados",
+    'UCSLAN_76' => "Administradores do site",
+    'UCSLAN_77' => "Os administradores de site principal",
+    'UCSLAN_78' => "Moderadores de fóruns e outras áreas",
+    'UCSLAN_80' => "Padrão",
+    'UCSLAN_81' => "Grupo",
+    'UCSLAN_82' => "Um grupo reúne uma série de aulas individuais",
+    'UCSLAN_83' => "Aulas em grupo",
+    'UCSLAN_85' => "Você tem atribuído a todas as classes disponíveis; por favor, reatribuir um que não está em uso",
+    'UCSLAN_86' => "Algumas configurações não permitida para classes de admin - eles tiverem sido definidos para os padrões.",
+    'UCSLAN_87' => "Recentemente se juntou aos utilizadores",
+    'UCSLAN_88' => "Robôs de busca identificados",
+    'UCSLAN_89' => "Verificado as classes são membros do grupo",
+    'UCSLAN_90' => "Você não pode editar certas classes de utilizador do sistema!",
+    'UCSLAN_91' => "Estrutura da classe",
+];
